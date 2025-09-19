9f0edac9b4a5c0c9158aac225023e111ae46ec6a
it's D <ifloriana2025@gmail.com>
2025-09-19 15:35:02 +0530

    working on ui

--- lib/ui/auth/login/login_screen.dart
+++ lib/ui/auth/login/login_screen.dart
@@ -24,6 +24,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         height: double.infinity,
         width: double.infinity,
